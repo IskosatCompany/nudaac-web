19b692be27961ad3a7b6e64c5be7736f989716bc
Alexandre Lavrador <alex1lavrador@gmail.com>
2023-01-13 00:20:41 +0000

    swrConfig

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeProvider } from "@material-ui/system";
 import Axios from "axios";
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { SWRConfig } from "swr";
 import "./App.css";
 import CasaPage from "./pages/CasaPage/CasaPage";
 import Home from "./pages/HomePage/Home";
@@ -9,8 +10,9 @@ import MoviesPage from "./pages/MoviesPage/MoviesPage";
 import NewsPage from "./pages/NewsPage/NewsPage";
 import NotesPage from "./pages/NotesPage/NotesPage";
 import { theme } from "./theme";
+import { fetcher } from "./utils";
 
-Axios.defaults.baseURL = "http://localhost:8080";
+Axios.defaults.baseURL = "http://localhost:8080/api/v1";
 
 function App() {
   useEffect(() => {
@@ -27,15 +29,23 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/news/" element={<NewsPage />} />
-          <Route path="/movies/" element={<MoviesPage />} />
-          <Route path="/casa/" element={<CasaPage />} />
-          <Route path="/notes/" element={<NotesPage />} />
-        </Routes>
-      </Router>
+      <SWRConfig
+        value={{
+          fetcher,
+          revalidateOnFocus: false,
+          errorRetryCount: 3,
+        }}
+      >
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/news/" element={<NewsPage />} />
+            <Route path="/movies/" element={<MoviesPage />} />
+            <Route path="/casa/" element={<CasaPage />} />
+            <Route path="/notes/" element={<NotesPage />} />
+          </Routes>
+        </Router>
+      </SWRConfig>
     </ThemeProvider>
   );
 }
